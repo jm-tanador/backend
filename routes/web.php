cf5c9ce5c6a824f170758936e53880bc62b6e929
jm-tanador <jmtanador7@gmail.com>
2026-07-29 13:43:24 +0800

    add temporary migration route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrations', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Migrations completed successfully!<br><pre>' . Artisan::output() . '</pre>';
+    } catch (\Exception $e) {
+        return 'Migration failed: ' . $e->getMessage();
+    }
+});
 
 Route::get('/', function () {
     return view('welcome');
